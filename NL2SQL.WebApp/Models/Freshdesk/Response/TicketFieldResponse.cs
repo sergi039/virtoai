ab033fba673aa4f43a35d5ab88853abc9c364cea
neketsch <n.scherban@softlist.com.ua>
2025-09-04 21:19:32 +0300

    feat(#11): create model for get info about statuses from freshdesk

---
+++ NL2SQL.WebApp/Models/Freshdesk/Response/TicketFieldResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NL2SQL.WebApp.Models.Freshdesk.Response
+{
+    public class TicketFieldResponse
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("choices")]
+        public Dictionary<string, object> Choices { get; set; } = new();
+    }
+}
