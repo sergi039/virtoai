a07ad1e7fc592a0136eb55559fd7c804f28104a1
neketsch <n.scherban@softlist.com.ua>
2025-09-04 21:18:49 +0300

    feat(#14): create dto and model and registration in mapper for remove logic

---
+++ NL2SQL.WebApp/Dtos/SqlTrainingData/Request/RemoveSqlTrainingDataDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NL2SQL.WebApp.Dtos.SqlTrainingData.Request
+{
+    public class RemoveSqlTrainingDataDto
+    {
+        public string NaturalLanguageQuery { get; set; }
+        public string GeneratedSql { get; set; }
+    }
+}
